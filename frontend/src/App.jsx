4f5fc416bfc8b48f787b31bc6af0d8d90c284ffa
KejZi <nikodem.bialik@gmail.com>
2025-03-05 21:39:11 +0100

    zmiana napisu v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Niko + Zjeba</h1>
+      <h1>Niko + Zjeba</h1><br></br>
+      <h2>Zapraszamy na reklamy</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
